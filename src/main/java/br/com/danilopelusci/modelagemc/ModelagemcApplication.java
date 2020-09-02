c53a1c4527ffab378b1dce8cab07d190be16db87
Danilo Pelusci <danilopelusci@gmail.com>
2020-09-01 22:40:32 -0300

    Criação de projeto

---
+++ src/main/java/br/com/danilopelusci/modelagemc/ModelagemcApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilopelusci.modelagemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danilopelusci/modelagemc/ModelagemcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilopelusci.modelagemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
