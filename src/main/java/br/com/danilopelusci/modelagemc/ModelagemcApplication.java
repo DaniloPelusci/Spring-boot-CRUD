c73cbe2393e8ce1942611ca005eed906eda65c58
Danilo Pelusci <danilopelusci@gmail.com>
2020-09-07 00:02:55 -0300

    produto e associacao muitos para muito

--- src/main/java/br/com/danilopelusci/modelagemc/ModelagemcApplication.java
+++ src/main/java/br/com/danilopelusci/modelagemc/ModelagemcApplication.java
@@ -1,13 +1,51 @@
 package br.com.danilopelusci.modelagemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemcApplication {
+import br.com.danilopelusci.modelagemc.domain.Categoria;
+import br.com.danilopelusci.modelagemc.domain.Produto;
+import br.com.danilopelusci.modelagemc.repositories.CategoriaRepository;
+import br.com.danilopelusci.modelagemc.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class ModelagemcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		Produto p1 = new Produto(null, "computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+	}
+	
 
 }
