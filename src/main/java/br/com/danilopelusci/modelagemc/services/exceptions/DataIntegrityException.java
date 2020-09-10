c51039d9e20cd09793304d761b78fc5cc2b6a50c
Danilo Pelusci <danilopelusci@gmail.com>
2020-09-09 22:04:27 -0300

    Deletando Categoria com DELETE

---
+++ src/main/java/br/com/danilopelusci/modelagemc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,19 @@
+package br.com.danilopelusci.modelagemc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	public DataIntegrityException(String msg,Throwable couse) {
+		super(msg, couse);
+	}
+	
+	
+
+}
