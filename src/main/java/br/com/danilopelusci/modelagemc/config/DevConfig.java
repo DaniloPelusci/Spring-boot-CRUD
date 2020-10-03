f09f86d3626b44bba3b80ac1904f4405e6a568e6
Danilo Pelusci <danilopelusci@gmail.com>
2020-10-03 12:35:45 -0300

    Criando profile dev

--- src/main/java/br/com/danilopelusci/modelagemc/config/DevConfig.java
+++ src/main/java/br/com/danilopelusci/modelagemc/config/DevConfig.java
@@ -2,7 +2,10 @@ package br.com.danilopelusci.modelagemc.config;
 
 import java.text.ParseException;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +13,20 @@ import org.springframework.context.annotation.Profile;
 import br.com.danilopelusci.modelagemc.services.DBService;
 
 @Configuration
-@Profile( "dev")
-public class TestConfig {
-	
+@Profile( "test")
+public class DevConfig {
 	@Autowired 
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instatiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instatiateTesteDatabase();
 		
 		return true;
