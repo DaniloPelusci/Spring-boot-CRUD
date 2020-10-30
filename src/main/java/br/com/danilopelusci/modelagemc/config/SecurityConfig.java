70f685df21b71f65cbe85edeace168073746d240
DaniloPelusci <danilopelusci@gmail.com>
2020-10-30 00:13:53 -0300

    autorizando endpoints para perfis especificos

--- src/main/java/br/com/danilopelusci/modelagemc/config/SecurityConfig.java
+++ src/main/java/br/com/danilopelusci/modelagemc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import br.com.danilopelusci.modelagemc.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
@@ -36,11 +38,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/h2-console/**"
 			
 	};
-	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			
 			"/produtos/**",
 			"/categorias/**",
+			"/clientes/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			
 			"/clientes/**"
 	};
 	
