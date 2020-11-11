9489babbc03435277d98c49121ffdc7dba81bd52
DaniloPelusci <danilopelusci@gmail.com>
2020-11-11 20:24:30 -0300

    esqueci a senha

--- src/main/java/br/com/danilopelusci/modelagemc/services/AbstractEmailService.java
+++ src/main/java/br/com/danilopelusci/modelagemc/services/AbstractEmailService.java
@@ -5,10 +5,11 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.danilopelusci.modelagemc.domain.Cliente;
 import br.com.danilopelusci.modelagemc.domain.Pedido;
 
-public abstract class AbstractEmailService implements EmailService{
-	
+public abstract class AbstractEmailService implements EmailService {
+
 	@Value("${default.sender}")
 	private String sender;
 
@@ -17,17 +18,34 @@ public abstract class AbstractEmailService implements EmailService{
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
-	//void sendEmail(SimpleMailMessage msg);
+	// void sendEmail(SimpleMailMessage msg);
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		// TODO Auto-generated method stub
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(obj.getCliente().getEmail());
 		sm.setFrom(sender);
-		sm.setSubject("Pedido Confirmado! Codigo: "+obj.getId());
+		sm.setSubject("Pedido Confirmado! Codigo: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
 		return sm;
 	}
+
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordemail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	private SimpleMailMessage prepareNewPasswordemail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha: ");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
+
 }
--- src/main/java/br/com/danilopelusci/modelagemc/services/EmailService.java
+++ src/main/java/br/com/danilopelusci/modelagemc/services/EmailService.java
@@ -4,11 +4,13 @@ package br.com.danilopelusci.modelagemc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.danilopelusci.modelagemc.domain.Cliente;
 import br.com.danilopelusci.modelagemc.domain.Pedido;
 
 public interface EmailService {
 	
 	void sendOrderConfirmatilEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage sm);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
